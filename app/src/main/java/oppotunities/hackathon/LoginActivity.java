456c60382ea54990f9b5aaa82fe4286a82a5168b
Hein Zaw Aung <allking99@gmail.com>
2014-09-17 18:58:47 +0630

    Login Activity

--- app/src/main/java/oppotunities/hackathon/LoginActivity.java
+++ app/src/main/java/oppotunities/hackathon/LoginActivity.java
@@ -28,10 +28,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-/**
- * A login screen that offers login via email/password.
 
- */
 public class LoginActivity extends Activity implements LoaderCallbacks<Cursor>{
 
 
